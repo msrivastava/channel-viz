6306d1c973506d2cadb4757e2cafc3846cbc2e8e
Mani Srivastava <mbs@ucla.edu>
2014-08-16 02:47:42 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'WGm7PjVAhzreO8UgvcrfYf6Zaxa3u9npTRvKz2zH4kCpnbS0', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [69618], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Home Dashboard', // Replaces Xively logo in the header
-		dataDuration	= '24hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
